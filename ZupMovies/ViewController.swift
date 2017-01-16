10ebe03ab0cb36005b81be5e5e7b2889ffc55fb2
Larissa Cavalcante <larissacavalcante@Larissas-MacBook-Air.local>
2017-01-16 18:28:07 -0300

    Initial Commit

---
+++ ZupMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZupMovies
+//
+//  Created by Larissa Cavalcante on 16/01/17.
+//  Copyright © 2017 Larissa Cavalcante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
